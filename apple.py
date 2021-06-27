a7f1fbcc9b3dab3a23df6fe326239285475a0b42
Chidvi7 <mandavilli.chidvilas.7@gmail.com>
2021-06-27 16:14:57 +0530

    ismart chidvi

---
+++ apple.py
@@ -0,0 +1,23 @@
+# Python program to check if the number is an Armstrong number or not
+
+# take input from the user
+num = int(input("Enter a number: "))
+
+# initialize sum
+sum = 1
+
+# find the sum of the cube of each digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
+
+
+#I am your friend and made changes.Rohan
